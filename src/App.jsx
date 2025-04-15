4a15f5f2cbb8c0f2288b92dd72b358aa14a35075
kiend <quangdang271105@gmail.com>
2025-04-15 15:46:38 +0200

    added the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom"
+
 //Pages
 import CreateProduct from './pages/CreateProduct'
 import HomePage from "./pages/HomePage"
@@ -6,9 +7,15 @@ import LogInPage from "./pages/LogInPage"
 import NotFoundPage from "./pages/NotFoundPage"
 import SignUpPage from "./pages/SignUpPage"
 
+//Components
+import NavBar from "./components/NavBar"
+import FooterComp from "./components/FooterComp"
+
 const App = () => {
   return (
+    <>
         <Router>
+          <NavBar />
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LogInPage />} />
@@ -16,7 +23,9 @@ const App = () => {
             <Route path="/create-product" element={<CreateProduct />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
+          <FooterComp />
         </Router>
+      </>
   )
 }
 
