f8339bf2a1abe81fb60bd8c0b8f3f703d7ae2449
fluffy29 <hmajzoub12@gmail.com>
2025-06-16 01:23:58 +0200

    feat: update CardComponent for improved UI and add fetchProducts error handling

--- src/utils/fetchProduts.js
+++ src/utils/fetchProduts.js
@@ -1,16 +1,16 @@
-const uri = "https://advanced-java-project.onrender.com/api/product/getAllProduct"
+const uri = "http://localhost:3001/api/product/getAllProduct";
 
 export const fetchProducts = async () => {
-    try {
-        const response = await fetch(uri)
-        if (!response.ok) {
-            throw new Error("Network response was not ok")
-        }
-        const data = await response.json()
-        const products = Array.isArray(data) ? data : data.products || [];
-        return products;
-    } catch (error) {
-        console.error("Error fetching products:", error)
-        throw error
+  try {
+    const response = await fetch(uri);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-}
+    const data = await response.json();
+    const products = Array.isArray(data) ? data : data.products || [];
+    return products;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    throw error;
+  }
+};
